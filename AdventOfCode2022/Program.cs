bff88cb239196dea36b5d1c2388c49131c6e65b6
marcelslom <95636743+marcelslom@users.noreply.github.com>
2022-12-08 19:36:54 +0100

    Day 7

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,7 +6,8 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            var solution = Day6.Init(@"Input/day6.txt");
+            //var solution = Day7.Init(@"test.txt");
+            var solution = Day7.Init(@"Input/day7.txt");
             PrintSolution(solution);
         }
 
---
+++ AdventOfCode2022/Solutions/Day7.cs
@@ -0,0 +1,134 @@
+﻿namespace AdventOfCode2022.Solutions
+{
+    internal class Day7 : ISolution
+    {
+        public int DayNumber => 7;
+
+        private Day7() { }
+
+        private string[] fileContent;
+
+        public static Day7 Init(string fileName)
+        {
+            return new Day7
+            {
+                fileContent = System.IO.File.ReadAllLines(fileName)
+            };
+        }
+
+        public string Part1()
+        {
+            var root = LoadFilesystem();
+            var size = root.Flatten().Concat(new List<Directory>() { root }).Where(x => x.Size < 100_000).Sum(x => x.Size);
+            return size.ToString();
+        }
+
+        private Directory LoadFilesystem()
+        {
+            var root = new Directory() { Name = "/" };
+            var current = root;
+            var lineCounter = 0;
+            while (lineCounter < fileContent.Length)
+            {
+                var line = fileContent[lineCounter];
+                if (line.StartsWith("$"))
+                {
+                    var splitted = line.Split(" ");
+                    var command = splitted[1];
+                    switch (command)
+                    {
+                        case "cd":
+                            var arg = splitted[2];
+                            if (arg == "/")
+                            {
+                                current = root;
+                            }
+                            else
+                            {
+                                current = arg == ".." ? current.Parent : current.Subdirectories.Single(x => x.Name == arg);
+                            }
+                            lineCounter++;
+                            break;
+                        case "ls":
+                            var lsLinesCounter = 1;
+                            while (true)
+                            {
+                                if (lineCounter + lsLinesCounter >= fileContent.Length)
+                                {
+                                    break;
+                                }
+
+                                var lsLine = fileContent[lineCounter + lsLinesCounter];
+                                if (lsLine.StartsWith("$"))
+                                {
+                                    break;
+                                }
+
+                                var ls = lsLine.Split(" ");
+                                var meta = ls[0];
+                                var name = ls[1];
+                                if (meta == "dir")
+                                {
+                                    var dir = new Directory() { Name = name, Parent = current };
+                                    current.Subdirectories.Add(dir);
+                                }
+                                else
+                                {
+                                    var file = new File() { Name = name, Size = int.Parse(meta), Parent = current };
+                                    current.Files.Add(file);
+                                }
+                                lsLinesCounter++;
+                            }
+                            lineCounter += lsLinesCounter;
+                            break;
+                    }
+                }
+                else
+                {
+                    throw new Exception($"Something went wrong, line {lineCounter}: {line} is parsed in main loop.");
+                }
+            }
+            return root;
+        }
+
+        public string Part2()
+        {
+            var root = LoadFilesystem();
+            var spaceToFree = root.Size - 40_000_000;
+            var minSpace = root.Flatten().Where(x => x.Size >= spaceToFree).Min(x => x.Size);
+            return minSpace.ToString();
+        }
+
+        private class Directory
+        {
+            public List<File> Files { get; } = new List<File>();
+            public List<Directory> Subdirectories { get; } = new List<Directory>();
+            public Directory Parent { get; init; }
+            public string Name { get; init; }
+            public int Size => Subdirectories.Sum(x => x.Size) + Files.Sum(x => x.Size);
+
+            public IEnumerable<Directory> Flatten()
+            {
+                return Subdirectories.Concat(Subdirectories.SelectMany(x => x.Flatten()));
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        private class File
+        {
+            public string Name { get; init; }
+            public int Size { get; init; }
+            public Directory Parent { get; init; }
+
+            public override string ToString()
+            {
+                return $"{Parent}/{Name} ({Size})";
+            }
+        }
+
+    }
+}
