24f0a823a428c83db8fe66a925a30b3fa8d99b55
marcelslom <95636743+marcelslom@users.noreply.github.com>
2023-01-28 23:43:08 +0100

    Day 10

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,16 +6,16 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            //var solution = Day9.Init(@"test.txt");
-            var solution = Day9.Init(@"Input/day9.txt");
+            //var solution = Day10.Init(@"test.txt");
+            var solution = Day10.Init(@"Input/day10.txt");
             PrintSolution(solution);
         }
 
         private static void PrintSolution(ISolution solution)
         {
             Console.WriteLine($"Day {solution.DayNumber}");
-            Console.WriteLine($"Part 1: {solution.Part1()}");
-            Console.WriteLine($"Part 2: {solution.Part2()}");
+            Console.WriteLine($"Part 1:\n{solution.Part1()}");
+            Console.WriteLine($"Part 2:\n{solution.Part2()}");
         }
     }
 }
---
+++ AdventOfCode2022/Solutions/Day10.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+
+namespace AdventOfCode2022.Solutions
+{
+    internal class Day10 : ISolution
+    {
+        public int DayNumber => 10;
+
+        private Day10() { }
+
+        private string[] fileContent;
+
+        public static Day10 Init(string fileName)
+        {
+            return new Day10
+            {
+                fileContent = System.IO.File.ReadAllLines(fileName)
+            };
+        }
+
+        public string Part1()
+        {
+            var signalStrengths = new List<int>();
+            var X = 1;
+            int clock = 0;
+            foreach(var instruction in fileContent)
+            {
+                var splitted = instruction.Split(' ');
+                switch(splitted[0])
+                {
+                    case "noop":
+                        clock++;
+                        if (CheckAndDump(clock, signalStrengths, X))
+                        {
+                            goto end;
+                        }
+                        break;
+                    case "addx":
+                        clock++;
+                        if (CheckAndDump(clock, signalStrengths, X))
+                        {
+                            goto end;
+                        }
+                        clock++;
+                        if (CheckAndDump(clock, signalStrengths, X))
+                        {
+                            goto end;
+                        }
+                        X += int.Parse(splitted[1]);
+                        break;
+                }
+            }
+            end:
+            return signalStrengths.Sum().ToString();
+        }
+
+        private static bool CheckAndDump(int clock, List<int> signalStrengths, int X)
+        {
+            if (DumpX(clock))
+            {
+                signalStrengths.Add(clock * X);
+            }
+            return clock >= 220;
+        }
+
+        private static bool DumpX(int clock)
+        {
+            return (clock - 20) % 40 == 0;
+        }
+
+        public string Part2()
+        {
+            var screenBuffer = new char[40 * 6];
+            var X = 1;
+            int clock = 0;
+            foreach (var instruction in fileContent)
+            {
+                var splitted = instruction.Split(' ');
+                switch (splitted[0])
+                {
+                    case "noop":
+                        screenBuffer[clock] = GetPixel(clock, X);
+                        clock++;
+                        break;
+                    case "addx":
+                        screenBuffer[clock] = GetPixel(clock, X);
+                        clock++;
+                        screenBuffer[clock] = GetPixel(clock, X);
+                        clock++;
+                        X += int.Parse(splitted[1]);
+                        break;
+                }
+            }
+
+            var lines = screenBuffer.Chunk(40);
+            var bob = new StringBuilder();
+            foreach (var line in lines)
+            {
+                bob.AppendLine(new string(line));
+            }
+            return bob.ToString();
+        }
+
+        private char GetPixel(int clock, int X)
+        {
+            var line = clock % 40;
+            return Math.Abs(line - X)<= 1 ? '#' : '.';
+        }
+
+    }
+}
